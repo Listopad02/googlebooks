1e6d5132f73b93a94e74828bd553be37bacd4029
Listopad02 <123ayetvbass@gmail.com>
2021-12-10 18:13:09 +0300

    books sorting added

--- src/App.js
+++ src/App.js
@@ -5,111 +5,129 @@ import axios from 'axios';
 import Loader from './components/Loader/Loader';
 
 class App extends React.Component {
-  state = {
-    defaultValue: 1,
-    book: '',
-    result: [],
-    apiKey: 'AIzaSyBeXETly2VZZIBjImMz-7kSNsAUdu2EUhk',
-    maxResults: 8,
-    loading: false
-  }
+	state = {
+		defaultValue: 'all',
+		defaultSortType: 'relevance',
+		book: '',
+		result: [],
+		apiKey: 'AIzaSyBeXETly2VZZIBjImMz-7kSNsAUdu2EUhk',
+		maxResults: 8,
+		loading: false,
+	}
 
-  changeHandler = e => {
-    this.setState({
-      book: e.target.value
-    })
-    console.log(this.state.book)
-  }
+	changeHandler = e => {
+		this.setState({
+			book: e.target.value
+		})
+		console.log(this.state.book)
+	}
 
-  submitHandler = e => {
-    e.preventDefault();
-    this.setState({
-      loading: true
-    })
+	submitHandler = e => {
+		e.preventDefault();
+		this.setState({
+			loading: true,
+			maxResults: 8
+		})
 
-    axios.get('https://www.googleapis.com/books/v1/volumes?q=' + this.state.book + "&key=" + this.state.apiKey + "&maxResults=30")
-    .then(data => {
-      console.log(data.data.items)
-      this.setState({
-        result: data.data.items,
-        loading: false
-      })
-    })
-  }
+		axios.get('https://www.googleapis.com/books/v1/volumes?q=' + this.state.book + "&orderBy=" + this.state.defaultSortType + "&key=" + this.state.apiKey + "&maxResults=40")
+		.then(data => {
+			console.log(data.data.items)
+    		this.setState({
+        		result: data.data.items,
+        		loading: false
+      		})
+    	})
+		.catch(error => {
+			console.log(error);
+		})
+  	}
 
-  selectChangeHandler = e => {
-    console.log(e.target.value);
-  }
+	selectChangeHandler = e => {
+    	this.setState({
+			defaultValue: +e.target.value
+		})
+  	}
 
-  pagination = () => {
-    this.setState({
-      maxResults: this.state.maxResults + 8
-    })
-  }
+	secondSelectChangeHandler = e => {
+		this.setState({
+			defaultSortType: e.target.value
+		})
+	}
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Online Google Library</h1>
-          <div className="App-InputForm">
-            <form onSubmit={this.submitHandler}>
-                <input type="text" 
-                       placeholder="Example: JavaScript" 
-                       onChange={this.changeHandler}
-                />
-                <button type="submit" onClick={this.submitHandler}>Search</button>
-            </form>
-          </div>
+  	pagination = () => {
+    	this.setState({
+      		maxResults: this.state.maxResults + 8
+    	})
+  	}
 
-          <div className="App-selects">
-            <Select label="Category:"
-                    value={this.state.defaultValue}
-                    onChange={this.selectChangeHandler}
-                    options={[
-                      {text: 'all', value: 1},
-                      {text: 'art', value: 2},
-                      {text: 'biography', value: 3},
-                      {text: 'computers', value: 4},
-                      {text: 'history', value: 5},
-                      {text: 'medical', value: 6},
-                      {text: 'poetry', value: 7},
-              ]} />
-            <Select label="Sort by:"
-                    value={this.state.defaultValue}
-                    onChange={this.selectChangeHandler}
-                    options={[
-                      {text: 'relevance', value: 1},
-                      {text: 'newest', value: 2}
-            ]} />
-          </div>
-        </header>
+  	render() {
+    	return (
+      		<div className="App">
+        		<header className="App-header">
+          			<h1>Online Google Library</h1>
+          			<div className="App-InputForm">
+            			<form onSubmit={this.submitHandler}>
+                			<input type="text" 
+                       		   	   placeholder="Example: JavaScript" 
+                       		       onChange={this.changeHandler}
+                			/>
+                			<button type="submit" onClick={this.submitHandler}>Search</button>
+            			</form>
+          			</div>
 
-        <main className="App-main">
-          {
-            this.state.loading 
-              ? <Loader />
-              : this.state.result.map((book, i) => {
-                if (i >= this.state.maxResults) {
-                  return null
-                } return (
-                <div className="App-main-container" key={i}>
-                  <img src={book.volumeInfo.imageLinks.thumbnail} alt={book.title} />
-                  <p><b>Category:</b> {book.volumeInfo.categories}</p>
-                  <p><b>Title:</b> {book.volumeInfo.title}</p>
-                  <p><b>Author:</b> {book.volumeInfo.authors + ' '}</p>
-                </div>
-              )})
-          }
-          {
-            this.state.result.length === 0
-              ? null
-              : <button onClick={this.pagination}>Show more</button>
-          }
-        </main>
-      </div>
-    )
-  }
+          			<div className="App-selects">
+            			<Select label="Category:"
+                    			value={this.state.defaultValue}
+                    			onChange={this.selectChangeHandler}
+                    			options={[
+                      				{text: 'all', value: 1},
+                      				{text: 'art', value: 2},
+                      				{text: 'biography', value: 3},
+                      				{text: 'computers', value: 4},
+                      				{text: 'history', value: 5},
+                      				{text: 'medical', value: 6},
+                      				{text: 'poetry', value: 7},
+              			]} />
+            			<Select label="Sort by:"
+                    			value={this.state.defaultSortType}
+                    			onChange={this.secondSelectChangeHandler}
+                    			options={[
+                      				{text: 'relevance', value: 'relevance'},
+                      				{text: 'newest', value: 'newest'}
+            			]} />
+          			</div>
+        		</header>
+
+        		<main className="App-main">
+					{
+						this.state.result.length === 0
+						? null
+						: <div className='Books-counter'><p>Books found: {this.state.result.length}</p></div>
+					}
+					{
+						this.state.loading 
+						? <Loader />
+						: this.state.result.map((book, i) => {
+							if (i >= this.state.maxResults) {
+							return null
+							} return (
+							<div className="App-main-container" key={i}>
+							<img src={book.volumeInfo.imageLinks === undefined ? "" : `${book.volumeInfo.imageLinks.thumbnail}`} alt={book.title} />
+							<p><b>Category:</b> {book.volumeInfo.categories}</p>
+							<p><b>Title:</b> {book.volumeInfo.title}</p>
+							<p><b>Author:</b> {book.volumeInfo.authors + ' '}</p>
+							</div>
+						)}) 
+					}
+					{
+						this.state.result.length === 0 || this.state.loading
+						? null
+						: <button onClick={this.pagination}>Show more</button>
+					}
+        		</main>
+      		</div>
+    	)
+  	}
 }
 
 export default App;
